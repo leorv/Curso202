560cf1468404710d4d04d9dc47fdaa94bfad8c33
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2021-08-20 20:46:14 -0300

    Commit inicial, solução sem o IComparable

---
+++ Curso202/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string path = @"C:\temp\nomes.txt";
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        List<string> list = new List<string>();
+
+        while (!sr.EndOfStream)
+        {
+            list.Add(sr.ReadLine());
+        }
+        list.Sort();
+        foreach (string str in list)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
